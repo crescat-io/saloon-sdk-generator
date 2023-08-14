186569ce7000896d8003da68d4c80a2e9f8f245d
Helge Sverre <helge.sverre@gmail.com>
2023-08-14 15:27:02 +0200

    wrong, should assign nullability based on param

--- src/Helpers/MethodGeneratorHelper.php
+++ src/Helpers/MethodGeneratorHelper.php
@@ -29,7 +29,7 @@ class MethodGeneratorHelper
             )
             ->addPromotedParameter($name)
             ->setType($parameter->type)
-            ->setNullable(false)
+            ->setNullable($parameter->nullable)
             ->setProtected();
 
         return $method;

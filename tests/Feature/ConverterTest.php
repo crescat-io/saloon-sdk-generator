1b3d1e46ce8823bd44b0a12b59001b943f995c85
Helge Sverre <helge.sverre@gmail.com>
2024-01-05 22:20:02 +0100

    test

--- tests/Feature/ConverterTest.php
+++ tests/Feature/ConverterTest.php
@@ -3,9 +3,8 @@
 use Crescat\SaloonSdkGenerator\Converters\OpenApiConverter;
 use Crescat\SaloonSdkGenerator\Exceptions\ConversionFailedException;
 
-it('will convert dintero-discounts.yaml from a swagger 2.0 file into an openapi 3.0.1 file', function () {
-    $input = file_get_contents(sample_path('dintero-discounts.yaml'));
-    OpenApiConverter::convert($input);
+it('fails on invalid input', function () {
+    OpenApiConverter::convert('invalid input');
 })->throws(ConversionFailedException::class);
 
 it('will convert tripletex.json from a swagger 2.0 file into an openapi 3.0.1 file', function () {
